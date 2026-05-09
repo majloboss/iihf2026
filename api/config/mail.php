5166930d54110e733662429d2af8e4b18afef640
Majloboss <miloslav.krchniak@gmail.com>
2026-05-09 10:05:05 +0200

    fix: SMTP config - smtp.websupport.sk port 465 SSL

--- api/config/mail.php
+++ api/config/mail.php
@@ -1,6 +1,6 @@
 <?php
-define('MAIL_HOST',     'mail.fellow.sk');
-define('MAIL_PORT',     587);
+define('MAIL_HOST',     'smtp.websupport.sk');
+define('MAIL_PORT',     465);
 define('MAIL_USER',     'betclub@fellow.sk');
 define('MAIL_PASS',     'WollefBetClub-2026');
 define('MAIL_FROM',     'betclub@fellow.sk');
